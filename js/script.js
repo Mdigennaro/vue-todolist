4bfd6c1bb21d88786b0619fd0eee4bd0b2c8a382
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-12 12:57:07 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const app = new Vue({
+
+  el: '#app',
+
+  data:{
+    todo:[
+      {
+        text: 'Alzarsi dal letto',
+        done: false
+      },
+      {
+        text: 'Cambiarsi',
+        done: false
+      },
+      {
+        text: 'Lavarsi',
+        done: false
+      },
+      {
+        text: 'Fare colazione',
+        done: false
+      },
+      {
+        text: 'Connettersi alla lezione',
+        done: false
+      },
+    ]
+  }
+
+})
